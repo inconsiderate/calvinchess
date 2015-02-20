68bbbbf63adeba29a2f6fdb2596a3a87affc7a31
Maggie Moss <maggiebmoss@gmail.com>
2015-02-20 22:35:14 +0000

    added rules for pawn movement

--- pawn.js
+++ pawn.js
@@ -1,6 +1,12 @@
 function makePawn(color, x, y){
   function pawnMove(item, pointer){
-
+     if (Math.abs(item.originY - item.y) > 100 || item.originX != item.x) {
+            game.add.tween(item).to({x: item.originX, y: item.originY}, 400, Phaser.Easing.Back.Out, true);
+      } else {
+        console.log("else");
+        item.originX = item.x;
+        item.originY = item.y;
+      }
   }
 
 	if (color == "white") {
@@ -11,7 +17,7 @@ function makePawn(color, x, y){
   pawn.inputEnabled = true;
   pawn.input.enableDrag();
   pawn.input.enableSnap(100, 100, false, true, 5, 5);
-  // pawn.events.onDragStop.add(pawnMove, pawn);
+  pawn.events.onDragStop.add(pawnMove, pawn);
   pawn.originX = pawn.x;
   pawn.originY = pawn.y;
   return pawn;
