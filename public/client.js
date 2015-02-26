349f4a653dfb0102d9fdf1543eb707cb3db3cc6c
MaggieMoss <maggiebmoss@gmail.com>
2015-02-25 18:30:43 -0800

    added more random rule changes

--- public/client.js
+++ public/client.js
@@ -397,11 +397,11 @@ function playerTwo(){
 }
 
 function rulesChange (ruleNumber) {
-  allRulesArray[0][1]();
+  allRulesArray[4][1]();
   var $calvinnameDiv = $('<span class="username"/>')
     .text('CalvinBot');
   var $messageRuleDiv = $('<spac class="messageBody">')
-    .text(allRulesArray[0][0]);
+    .text(allRulesArray[4][0]);
   var $ruleChangeDiv = $('<li class="message"/>')
     .data('username', 'CalvinBot')
     .append($calvinnameDiv, $messageRuleDiv);
