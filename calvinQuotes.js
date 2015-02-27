468fa46fc2a2df6cad229f4d9668535e82a7605d
inconsiderate <meiskant@gmail.com>
2015-02-26 17:10:48 -0800

    some random SURPRISE changes

---
+++ calvinQuotes.js
@@ -0,0 +1,37 @@
+"Life's a lot more fun when you aren't responsible for your actions."
+
+"A day can really slip by when you're deliberately avoiding what you're supposed to do."
+
+"Not having my emotions manipulated is such a weird experience."
+
+"There's never enough time to do all the nothing you want."
+
+"That's one of the remarkable things about life. It's never so bad that it can't get worse."
+
+"This is so cool I have to go to the bathroom!"
+
+"I say, if your knees aren't green by the end of the day, you ought to seriously re-examine your life."
+
+"Weekends don't count unless you spend them doing something completely pointless."
+
+"The only permanent rule in calvinball is that you can't play the same way twice!"
+
+"This one's tricky. You have to use imaginary numbers, like eleventeen..."
+
+"I propose we leave math to the machines and go play outside."
+
+"It's hard to be religious when certain people are never incinerated by bolts of lightning." 
+
+"One can certainly imagine the myriad of uses for a hand-held iguana maker."
+
+"Every minute outside and awake is a good minute."
+
+"If you couldn't find any weirdness, maybe we'll just have to make some!"
+
+"If people could put rainbows in zoos, they'd do it."
+
+"I don't know if I can tolerate that much tolerance."
+
+"Where do we keep all our chainsaws, Mom?"
+
+"You rate especialy low among tigers and six year old white males."
